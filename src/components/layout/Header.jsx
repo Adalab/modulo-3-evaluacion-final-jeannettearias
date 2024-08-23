88cedbf0f96d2fe4bded64f73c12a8eabf8313c6
jeannettearias <jeannettearias@icloud.com>
2024-08-23 19:22:52 +0200

    change image path

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
                 <div>
                     <img
                         className='header__img'
-                        src="../../img/harry-potter-logo-32524.png" alt="harry-potter-logo" />
+                        src="./img/harry-potter-logo-32524.png" alt="harry-potter-logo" />
                 </div>
             </header>
         </>
